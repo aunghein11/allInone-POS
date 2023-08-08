d9cf92d492549077e9c2f0de62795820aefb23bc
Aung Hein <heina8649@example.com>
2023-08-08 16:02:03 +0630

    feat: added src/db/db.ts in backend folder and joined database and frontend for regristration

--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { db } from "./src/db/db";
 const port = 5000;
 
 const app = express();
@@ -9,8 +10,21 @@ app.use(cors());
 
 app.post("/auth/register", async (req: Request, res: Response) => {
   const { name, email, password } = req.body;
-  res.send({ name, email, password });
-  console.log("request body", req.body);
+  if (!name || !email || !password) return res.sendStatus(400);
+
+  try {
+    const text =
+      "INSERT INTO users(name, email, password) VALUES($1, $2, $3) RETURNING*";
+    const values = [name, email, password];
+    const userResult = await db.query(text, values);
+    const user = userResult.rows[0];
+    delete user.password;
+    res.send(user);
+    console.log("request body", user);
+  } catch (error) {
+    console.log("error:....", error);
+    res.sendStatus(500);
+  }
 });
 
 app.listen(port, () => {
